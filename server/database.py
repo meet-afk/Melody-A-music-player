cbc1a647d27b9b2e48b81cdc51ee5dbd30b9fc54
Meet <meetsingh4150@gmail.com>
2026-03-19 16:08:11 +0530

    1

--- server/database.py
+++ server/database.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Meet@localhost:5432/musicapp")
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
